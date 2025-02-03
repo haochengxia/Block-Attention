c2881ea2efe4d373491e829111d45c06ac5c624e
Percy <xhc_1007@163.com>
2025-02-03 04:20:35 -0600

    add more results (#1)

---
+++ bench/instruct/instruct_new.sh
@@ -0,0 +1,17 @@
+# instruct_models="deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+instruct_models="meta-llama/Llama-3.1-8B-Instruct"
+instruct_models="meta-llama/Meta-Llama-3-8B-Instruct"
+instruct_models="meta-llama/Llama-3.2-3B-Instruct"
+
+datasets="2wiki hqa tqa nq"
+for im in $instruct_models; do
+    for ds in $datasets; do
+        echo "Run $im over $ds";
+        echo "Run $im over $ds" >> temp_result_block;
+        echo "Run $im over $ds" >> temp_result_full;
+	python3 block_generate.py --model_name $im --input_file cache/${ds}_eval/dataset --output_file cache/benchmark/$im/${ds}_block;
+	python3 eval.py --input cache/benchmark/$im/${ds}_block >> temp_result_block;
+	python3 full_generate.py --model_name $im --input_file cache/${ds}_eval/dataset --output_file cache/benchmark/$im/${ds}_full;
+	python3 eval.py --input cache/benchmark/$im/${ds}_full >> temp_result_full;
+    done
+done
